9e6a4914d2b03c1e56620d1376d4c4a709c30b19
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-10-22 22:49:32 +0200

    heroku fix errors

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.use(cors({
 }))
 
 //Vercel
-app.get('/', (_req:Request,res:Response) => {
+app.use('/', (_req:Request,res:Response) => {
   res.setHeader('Access-Control-Allow-Origin', '*');  
   res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Methods','Content-Type');
