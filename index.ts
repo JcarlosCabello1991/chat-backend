ddd58915c2a40fd61c9f07deebfa2d614872d095
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-10-22 12:44:06 +0200

    vercel back config

--- index.ts
+++ index.ts
@@ -12,6 +12,9 @@ app.use(cors({
 
 //Vercel
 app.use('/', (_req:Request,res:Response) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');  
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type');
   res.send("Inicio")
 })
 
